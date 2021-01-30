2e322fcaa9205c04adac4c0ef8a73cd236d1e4f3
LizanoLeo123 <leoln123@live.com>
2021-01-29 19:52:41 -0600

    Created Test Scene and player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private Rigidbody2D rb;
+
+    Vector2 movementDir;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Input
+        movementDir.x = Input.GetAxisRaw("Horizontal");
+        movementDir.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        //Movement
+        Move();
+    }
+
+    void Move()
+    {
+        rb.MovePosition(rb.position + movementDir * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    void Dash()
+    {
+
+    }
+
+    void Atack()
+    {
+
+    }
+}
