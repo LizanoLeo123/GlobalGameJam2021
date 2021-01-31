5b1c64d55c57f0e3d89368fa70358e86290f4a07
Andrey Rojas <andrey20ag95@gmail.com>
2021-01-31 02:42:24 -0600

    Spawn de las verduras, tiene entrada de cantidad en la interfaz y debe colocar las areas del terreno en la lista de Areas.

---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Spawn : MonoBehaviour
+{
+    public GameObject Vegetable;
+    public float Speed;
+    public int cant;
+    List<GameObject> allVegetable = new List<GameObject>();
+    public List<GameObject> Areas = new List<GameObject>();
+
+
+    private void Start()
+    {
+        InvokeRepeating("Generate", 0, Speed);
+
+    }
+
+    void Generate()
+    {
+        
+
+        while (cant > 0)
+        {
+            int areaRand = Random.Range(0, Areas.Count);
+
+            float posX = Areas[areaRand].transform.position.x;
+            float posY = Areas[areaRand].transform.position.y;
+
+            BoxCollider2D boxArea = Areas[areaRand].GetComponent<BoxCollider2D>();
+            float extendX = boxArea.bounds.extents.x;
+            float extendY = boxArea.bounds.extents.y;
+
+
+            float rangeX1 = posX - extendX;
+            float rangeX2 = posX + extendX;
+
+            float rangeY1 = posY - extendY;
+            float rangeY2 = posY + extendY;
+
+
+            float x = Random.Range(rangeX1, rangeX2);
+            float y = Random.Range(rangeY1, rangeY2);
+
+            
+
+            Vector2 Target = new Vector2(x, y);
+
+            GameObject thisFood = Instantiate(Vegetable, Target, Quaternion.identity);
+            allVegetable.Add(thisFood);
+
+            cant -= 1;
+        }
+        
+    }
+}
