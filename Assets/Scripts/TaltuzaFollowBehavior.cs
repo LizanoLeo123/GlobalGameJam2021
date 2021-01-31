76f8a365e64be5a72b2a233838a9ece7dcc21f69
Ricardo Leon <ricardo.cr023@gmail.com>
2021-01-31 10:37:04 -0600

    Finished Taltuza Patrolling and Attacking AI. Also implemented 5 patrol spots.

--- Assets/Scripts/TaltuzaFollowBehavior.cs
+++ Assets/Scripts/TaltuzaFollowBehavior.cs
@@ -4,34 +4,40 @@ using UnityEngine;
 
 public class TaltuzaFollowBehavior : StateMachineBehaviour
 {
+    private Transform patrolLocation;
     private Transform playerPos;
     public float speed;
+    public int patrolNumber;
 
     // Start
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         playerPos = GameObject.FindGameObjectWithTag("Player").transform;
+        patrolLocation = GameObject.Find("TaltuzaPatrolSpots" + patrolNumber.ToString()).transform;
     }
 
     // Update
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         //animator.transform.position = Vector2.MoveTowards(animator.transform.position, playerPos.position, speed * Time.deltaTime);
-        Debug.Log((playerPos.position - animator.transform.position).normalized);
+        //Debug.Log((playerPos.position - animator.transform.position).normalized);
         Vector2 _vector = (playerPos.position - animator.transform.position).normalized;
         animator.GetComponent<Rigidbody2D>().velocity = _vector * speed;
+        Debug.Log(Vector2.Distance(playerPos.position,patrolLocation.position));
 
-        //if (Input.GetKeyDown(KeyCode.Space))
-        if (Input.GetKeyDown(KeyCode.Space))
+        //if (Input.GetKeyDown(KeyCode.L))
+        if (Vector2.Distance(playerPos.position, patrolLocation.position) > 5.0f)
         {
             animator.SetBool("isFollowing", false);
             animator.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-        }
-
-        if (Input.GetKeyDown(KeyCode.P))
-        {
             animator.SetBool("isPatrolling", true);
         }
+
+        //if (Vector2.Distance(playerPos.position, patrolLocation.position) > 5.0f)
+        //if (Input.GetKeyDown(KeyCode.P))
+        //{
+        //    animator.SetBool("isPatrolling", true);
+        //}
     }
 
     //Stops
@@ -40,15 +46,5 @@ public class TaltuzaFollowBehavior : StateMachineBehaviour
 
     }
 
-    // OnStateMove is called right after Animator.OnAnimatorMove()
-    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that processes and affects root motion
-    //}
 
-    // OnStateIK is called right after Animator.OnAnimatorIK()
-    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that sets up animation IK (inverse kinematics)
-    //}
 }
--- Assets/TaltuzaPatrollingBehavior.cs
+++ Assets/TaltuzaPatrollingBehavior.cs
@@ -6,17 +6,26 @@ public class TaltuzaPatrollingBehavior : StateMachineBehaviour
 {
     private TaltuzaPatrolSpots patrol;
     public float speed;
+    public int patrolNumber;
     private int randomSpot;
+    private Transform patrolLocation;
+    private Transform playerPos;
 
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        patrol = GameObject.FindGameObjectWithTag("PatrolSpots").GetComponent<TaltuzaPatrolSpots>();
+        patrol = GameObject.Find("TaltuzaPatrolSpots" + patrolNumber.ToString()).GetComponent<TaltuzaPatrolSpots>();
         randomSpot = Random.Range(0, patrol.patrolPoints.Length);
+        patrolLocation = GameObject.Find("TaltuzaPatrolSpots" + patrolNumber.ToString()).transform;
+        playerPos = GameObject.FindGameObjectWithTag("Player").transform;
+
     }
 
 
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+
+        animator.SetBool("isPatrolling", true);
+
         if (Vector2.Distance(animator.transform.position, patrol.patrolPoints[randomSpot].position) > 0.2f)
         {
             //animator.transform.position = Vector2.MoveTowards(animator.transform.position, patrol.patrolPoints[randomSpot].position, speed * Time.deltaTime);
@@ -29,9 +38,11 @@ public class TaltuzaPatrollingBehavior : StateMachineBehaviour
             randomSpot = Random.Range(0, patrol.patrolPoints.Length);
         }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        //if (Input.GetKeyDown(KeyCode.Space))
+        if (Vector2.Distance(playerPos.position, patrolLocation.position) < 5.0f)
         {
             animator.SetBool("isPatrolling", false);
+            animator.SetBool("isFollowing", true);
         }
 
     }
@@ -42,3 +53,4 @@ public class TaltuzaPatrollingBehavior : StateMachineBehaviour
 
     }
 }
+
